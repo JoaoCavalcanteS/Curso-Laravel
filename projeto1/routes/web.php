a60f18c8d139b1154c90ef00085a53e7052e8518
João Cavalcante <joaovitorcavalcante2012@gmail.com>
2023-01-04 00:17:00 -0200

    Testando Views

--- projeto1/routes/web.php
+++ projeto1/routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/home', function () {
+//     return view('welcome');
+// });
+// Route::view('/jogos.blade','jogos');
+// Route::get('/jogos', function(){
+//     return "curso de laravel do gustavo";
+// });
+// Route::views('/jogos', 'jogos',['name'=> 'GTA']);
+Route::get('/jogos/{name}', function($name){
+    return view('jogos', ['nomeJogo'=>$name]);
 });
